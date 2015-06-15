d3a4b573b5a4222afeba74ae2ec534571c221b1b
nbIxMaN <Shulllik@gmail.com>
2015-06-15 16:13:46 +0300

    some tests are now working

--- UnitTests/UnitTests/UnitTest1.cs
+++ UnitTests/UnitTests/UnitTest1.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using UnitTests.PixService;
+using System.ServiceModel;
 
 namespace UnitTests
 {
@@ -42,15 +43,138 @@ namespace UnitTests
             client.Close();
         }
         [Test]
+        public void addExample()
+        {
+            var rnd = new System.Random(DateTime.Now.GetHashCode());
+            using (var service = new PixServiceClient())
+            {
+                service.AddPatient("8CDE415D-FAB7-4809-AA37-8CDD70B1B46C", "1.2.643.5.1.13.3.25.78.118", new PatientDto
+                {
+                    IdPatientMis = rnd.Next().ToString(),
+                    FamilyName = "165",
+                    GivenName = "1651",
+                    Sex = 1,
+                    BirthDate = DateTime.Now,
+                    Documents = new DocumentDto[]
+                    {
+                        new DocumentDto
+                        {
+                            DocS = "5465",
+                            DocN = "6546",
+                            ProviderName = "6546",
+                            IdDocumentType = 223
+                        },
+                    },
+                });
+            }
+            //string guid = "8CDE415D-FAB7-4809-AA37-8CDD70B1B46C";
+            //string xmlns = "1.2.643.5.1.13.3.25.78.118";
+            //PatientDto patient = new PatientDto();
+            //AddressDto address1 = new AddressDto();
+            //address1.Appartment = "1000";
+            //address1.Building = "454";
+            //address1.City = "0100000000000";
+            //address1.GeoData = "3.072812,-79.040128";
+            //address1.IdAddressType = 2;
+            //address1.PostalCode = 454100;
+            //address1.Street = "01000001000000100";
+            //address1.StringAddress = "454100, город Челябинск, ул. Ленина, дом 454, квартира 1000";
+            //AddressDto address2 = new AddressDto();
+            //address2.Appartment = "002";
+            //address2.Building = "202";
+            //address2.City = "0100000000000";
+            //address2.GeoData = "43.072812,-79.040128";
+            //address2.IdAddressType = 8;
+            //address2.PostalCode = 198000;
+            //address2.Street = "01000001000000100";
+            //address2.StringAddress = "198000, город Санкт-Петербург, ул. Ленина, дом 202, квартира 002";
+            //AddressDto[] adrs = new AddressDto[2] {address1, address2};
+            //patient.Addresses = adrs;
+            //patient.BirthDate = new DateTime(1972, 11, 29);
+            //BirthPlaceDto birthPlace = new BirthPlaceDto();
+            //birthPlace.City = "Москва";
+            //birthPlace.Country = "Россия";
+            //birthPlace.Region = "Московская область";
+            //patient.BirthPlace = birthPlace;
+            //ContactDto contact1 = new ContactDto();
+            //contact1.ContactValue = "8-111-111-11-11";
+            //contact1.IdContactType = 1;
+            //ContactDto contact2 = new ContactDto();
+            //contact2.ContactValue = "111-11-11";
+            //contact2.IdContactType = 2;
+            //ContactDto[] conts = new ContactDto[2] {contact1, contact2};
+            //patient.Contacts = conts;
+            //DocumentDto document1 = new DocumentDto();
+            //document1.DocN = "59165576238";
+            //document1.DocS = "";
+            //document1.DocumentName = "";
+            //document1.IdDocumentType = 223;
+            //document1.IssuedDate = new DateTime (2010, 12, 22);
+            //document1.ProviderName = "ПФР";
+            //DocumentDto document2 = new DocumentDto();
+            //document2.DocN = "88885916";
+            //document2.DocS = "4444";
+            //document2.IdDocumentType = 14;
+            //document2.IssuedDate = new DateTime(2010, 12, 22);
+            //document2.ProviderName = "УФМС";
+            //document2.RegionCode = "078";
+            //DocumentDto document3 = new DocumentDto();
+            //document3.DocN = "225916";
+            //document3.DocS = "AA";
+            //document3.ExpiredDate = new DateTime(2020, 01, 01);
+            //document3.IdDocumentType = 226;
+            //document3.IdProvider = 23807;
+            //document3.IssuedDate = new DateTime(2010, 12, 22);
+            //document3.ProviderName = "ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ \"МУНИЦИПАЛЬНАЯ СТРАХОВАЯ КОМПАНИЯ Г.КРАСНОДАРА - МЕДИЦИНА\"";
+            //document3.RegionCode = "078";
+            //DocumentDto document4 = new DocumentDto();
+            //document4.DocN = "1212121212125916";
+            //document4.DocS = "";
+            //document4.DocumentName = "";
+            //document4.ExpiredDate = new DateTime(2020, 01, 01);
+            //document4.IdDocumentType = 228;
+            //document4.IdProvider = 23807;
+            //document4.IssuedDate = new DateTime(2010, 12, 22);
+            //document4.ProviderName = "ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ \"МУНИЦИПАЛЬНАЯ СТРАХОВАЯ КОМПАНИЯ Г.КРАСНОДАРА - МЕДИЦИНА\"";
+            //document4.RegionCode = "078";
+            //DocumentDto[] docs = new DocumentDto[4] {document1, document2, document3, document4};
+            //patient.Documents = docs;
+            //patient.FamilyName = "Сидоров";
+            //patient.GivenName = "Данила";
+            //patient.IdBloodType = 8;
+            //patient.IdLivingAreaType = 2;
+            //patient.IdPatientMis = "IdentificatorOfPatient";
+            //JobDto job = new JobDto();
+            //job.CompanyName = "ООО Вкусный Апельсин";
+            //job.DateEnd = new DateTime(2100, 01, 01);
+            //job.DateStart = new DateTime(1900, 01, 01);
+            //job.OgrnCode = "1234567890123";
+            //job.Position = "кассир";
+            //job.Sphere = "Обслуживание";
+            //patient.Job = job;
+            //patient.MiddleName = "Викторович";
+            //PrivilegeDto privilage = new PrivilegeDto();
+            //privilage.DateEnd = new DateTime(2020, 02, 02);
+            //privilage.DateStart = new DateTime(2010, 01, 01);
+            //privilage.IdPrivilegeType = 10;
+            //patient.Privilege = privilage;
+            //patient.Sex = 3;
+            //patient.SocialGroup = 4;
+            //patient.SocialStatus = 26;
+            //PixServiceClient client = new PixServiceClient();
+            //client.AddPatient(guid, xmlns, patient);
+        }
+        [Test]
         public void PatientWithRequiredFields()
         {
+            //FaultException ex = FaultException();
             PixServiceClient client = new PixServiceClient();
             PatientDto patient = new PatientDto();
-            patient.IdPatientMis = "123";
-            patient.FamilyName = "Petrov";
-            patient.GivenName = "Ivan";
-            patient.Sex = 1;
-            patient.BirthDate = new DateTime(1995, 4, 12, 13, 33, 11);
+            patient.IdPatientMis = new Random(DateTime.Now.GetHashCode()).Next().ToString();
+            patient.FamilyName = "Петров";
+            patient.GivenName = "Иван";
+            patient.Sex = 3;
+            patient.BirthDate = new DateTime(1995, 8, 2);
             client.AddPatient("8CDE415D-FAB7-4809-AA37-8CDD70B1B46C", "1.2.643.5.1.13.3.25.78.118", patient);
             client.Close();
         }
@@ -59,16 +183,16 @@ namespace UnitTests
         {
             PixServiceClient client = new PixServiceClient();
             PatientDto patient = new PatientDto();
-            patient.IdPatientMis = "123";
-            patient.FamilyName = "Petrov";
-            patient.GivenName = "Ivan";
+            patient.IdPatientMis = new Random(DateTime.Now.GetHashCode()).Next().ToString();
+            patient.FamilyName = "165";
+            patient.GivenName = "1651";
             patient.Sex = 1;
-            patient.BirthDate = new DateTime(1995, 4, 12, 13, 33, 11);
+            patient.BirthDate = DateTime.Now;            
             DocumentDto document = new DocumentDto();
-            document.IdDocumentType = 14;
-            document.DocS = "1111 111111";
+            document.DocS = "5465";
             document.DocN = "111-111-111 11";
-            document.ProviderName = "UFMS Sity";
+            document.ProviderName = "6546";
+            document.IdDocumentType = 223;
             DocumentDto[] docs = new DocumentDto[1] { document };
             patient.Documents = docs;
             client.AddPatient("8CDE415D-FAB7-4809-AA37-8CDD70B1B46C", "1.2.643.5.1.13.3.25.78.118", patient);
@@ -80,16 +204,16 @@ namespace UnitTests
             string correctResult = "Параметр DocN контейнера Documents заполнен некорректно";
             PixServiceClient client = new PixServiceClient();
             PatientDto patient = new PatientDto();
-            patient.IdPatientMis = "123";
-            patient.FamilyName = "Petrov";
-            patient.GivenName = "Ivan";
+            patient.IdPatientMis = new Random(DateTime.Now.GetHashCode()).Next().ToString();
+            patient.FamilyName = "165";
+            patient.GivenName = "1651";
             patient.Sex = 1;
-            patient.BirthDate = new DateTime(1995, 4, 12, 13, 33, 11);
+            patient.BirthDate = DateTime.Now;
             DocumentDto document = new DocumentDto();
-            document.IdDocumentType = 14;
-            document.DocS = "1111 111111";
-            document.DocN = "111-111-111 11234";
-            document.ProviderName = "UFMS Sity";
+            document.DocS = "5465";
+            document.DocN = "111-111-111 112345";
+            document.ProviderName = "6546";
+            document.IdDocumentType = 223;
             DocumentDto[] docs = new DocumentDto[1] { document };
             patient.Documents = docs;
             try
@@ -109,11 +233,18 @@ namespace UnitTests
             string correctResult = "Попытка повторного добавления пациента";
             PixServiceClient client = new PixServiceClient();
             PatientDto patient = new PatientDto();
-            patient.IdPatientMis = "123";
-            patient.FamilyName = "Petrov";
-            patient.GivenName = "Ivan";
+            patient.IdPatientMis = new Random(DateTime.Now.GetHashCode()).Next().ToString();
+            patient.FamilyName = "165";
+            patient.GivenName = "1651";
             patient.Sex = 1;
-            patient.BirthDate = new DateTime(1995, 4, 12, 13, 33, 11);
+            patient.BirthDate = DateTime.Now;
+            DocumentDto document = new DocumentDto();
+            document.DocS = "5465";
+            document.DocN = "111-111-111 11";
+            document.ProviderName = "6546";
+            document.IdDocumentType = 223;
+            DocumentDto[] docs = new DocumentDto[1] { document };
+            patient.Documents = docs;
             client.AddPatient("8CDE415D-FAB7-4809-AA37-8CDD70B1B46C", "1.2.643.5.1.13.3.25.78.118", patient);
             try
             {
@@ -131,11 +262,18 @@ namespace UnitTests
         {
             PixServiceClient client = new PixServiceClient();
             PatientDto patient = new PatientDto();
-            patient.IdPatientMis = "123";
-            patient.FamilyName = "Petrov";
-            patient.GivenName = "Ivan";
+            patient.IdPatientMis = new Random(DateTime.Now.GetHashCode()).Next().ToString();
+            patient.FamilyName = "165";
+            patient.GivenName = "1651";
             patient.Sex = 1;
-            patient.BirthDate = new DateTime(1995, 4, 12, 13, 33, 11);
+            patient.BirthDate = DateTime.Now;
+            DocumentDto document = new DocumentDto();
+            document.DocS = "5465";
+            document.DocN = "111-111-111 11";
+            document.ProviderName = "6546";
+            document.IdDocumentType = 223;
+            DocumentDto[] docs = new DocumentDto[1] { document };
+            patient.Documents = docs;
             BirthPlaceDto birthPlace = new BirthPlaceDto();
             birthPlace.Country = "Russia";
             birthPlace.Region = "Center";
@@ -150,11 +288,18 @@ namespace UnitTests
             string correctResult = "Параметр Sex контейнера Patient заполнен некорректно";
             PixServiceClient client = new PixServiceClient();
             PatientDto patient = new PatientDto();
-            patient.IdPatientMis = "123";
-            patient.FamilyName = "Petrov";
-            patient.GivenName = "Ivan";
+            patient.IdPatientMis = new Random(DateTime.Now.GetHashCode()).Next().ToString();
+            patient.FamilyName = "165";
+            patient.GivenName = "1651";
             patient.Sex = 144;
-            patient.BirthDate = new DateTime(1995, 4, 12, 13, 33, 11);
+            patient.BirthDate = DateTime.Now;
+            DocumentDto document = new DocumentDto();
+            document.DocS = "5465";
+            document.DocN = "111-111-111 11";
+            document.ProviderName = "6546";
+            document.IdDocumentType = 223;
+            DocumentDto[] docs = new DocumentDto[1] { document };
+            patient.Documents = docs;
             try
             {
                 client.AddPatient("8CDE415D-FAB7-4809-AA37-8CDD70B1B46C", "1.2.643.5.1.13.3.25.78.118", patient);
@@ -171,11 +316,18 @@ namespace UnitTests
         {
             PixServiceClient client = new PixServiceClient();
             PatientDto patient = new PatientDto();
-            patient.IdPatientMis = "123";
-            patient.FamilyName = "Petrov";
-            patient.GivenName = "Ivan";
+            patient.IdPatientMis = new Random(DateTime.Now.GetHashCode()).Next().ToString();
+            patient.FamilyName = "165";
+            patient.GivenName = "1651";
             patient.Sex = 1;
-            patient.BirthDate = new DateTime(1995, 4, 12, 13, 33, 11);
+            patient.BirthDate = DateTime.Now;
+            DocumentDto document = new DocumentDto();
+            document.DocS = "5465";
+            document.DocN = "111-111-111 11";
+            document.ProviderName = "6546";
+            document.IdDocumentType = 223;
+            DocumentDto[] docs = new DocumentDto[1] { document };
+            patient.Documents = docs;
             PrivilegeDto privilage = new PrivilegeDto();
             privilage.DateStart = new DateTime(1996, 9, 13);
             privilage.DateEnd = new DateTime(2005, 7, 14);
@@ -190,11 +342,18 @@ namespace UnitTests
             string correctResult = "Значение параметра DateStart не может быть больше значения параметра DateEnd контейнера Privilege";
             PixServiceClient client = new PixServiceClient();
             PatientDto patient = new PatientDto();
-            patient.IdPatientMis = "123";
-            patient.FamilyName = "Petrov";
-            patient.GivenName = "Ivan";
+            patient.IdPatientMis = new Random(DateTime.Now.GetHashCode()).Next().ToString();
+            patient.FamilyName = "165";
+            patient.GivenName = "1651";
             patient.Sex = 1;
-            patient.BirthDate = new DateTime(1995, 4, 12, 13, 33, 11);
+            patient.BirthDate = DateTime.Now;
+            DocumentDto document = new DocumentDto();
+            document.DocS = "5465";
+            document.DocN = "111-111-111 11";
+            document.ProviderName = "6546";
+            document.IdDocumentType = 223;
+            DocumentDto[] docs = new DocumentDto[1] { document };
+            patient.Documents = docs;
             PrivilegeDto privilage = new PrivilegeDto();
             privilage.DateStart = new DateTime(2005, 9, 13);
             privilage.DateEnd = new DateTime(1996, 7, 14);
@@ -216,25 +375,16 @@ namespace UnitTests
         {
             PixServiceClient client = new PixServiceClient();
             PatientDto patient = new PatientDto();
-            patient.IdPatientMis = "123";
-            patient.FamilyName = "Petrov";
-            patient.MiddleName = "Vasilevich";
-            patient.GivenName = "Ivan";
+            patient.IdPatientMis = new Random(DateTime.Now.GetHashCode()).Next().ToString();
+            patient.FamilyName = "165";
+            patient.GivenName = "1651";
             patient.Sex = 1;
-            patient.BirthDate = new DateTime(1995, 8, 2);
-            patient.IdBloodType = 1;
-            patient.IdLivingAreaType = 1;
-            patient.SocialStatus = 1;
-            patient.SocialGroup = 2;
+            patient.BirthDate = DateTime.Now;
             DocumentDto document = new DocumentDto();
-            document.IdDocumentType = 14;
-            document.DocS = "1111 111111";
-            document.DocN = "111-111-111 11234";
-            document.ExpiredDate = new DateTime(1992, 4, 12);
-            document.IssuedDate = new DateTime(2018, 3, 3);
-            document.IdProvider = 1;
-            document.ProviderName = "UFMS Sity";
-            document.RegionCode = "1";
+            document.DocS = "5465";
+            document.DocN = "111-111-111 11";
+            document.ProviderName = "6546";
+            document.IdDocumentType = 223;
             AddressDto address = new AddressDto();
             address.IdAddressType = 1;
             address.StringAddress = "MyAddress";
